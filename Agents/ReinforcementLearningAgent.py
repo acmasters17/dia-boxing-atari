0f2b054ff4d57b9a5c9de697dac3a490d2c1f16f
Adam <mastersacm17@gmail.com>
2022-04-29 15:30:48 +0100

    Preparing 10 mil run

--- Agents/ReinforcementLearningAgent.py
+++ Agents/ReinforcementLearningAgent.py
@@ -2,12 +2,13 @@ import random
 from gym import Env
 from stable_baselines3 import A2C
 from Agents.Agent import Agent
+from stable_baselines3 import ACER
 
 # Agent will load best model
 class ReinforcementLearningAgent(Agent):
     def __init__(self):
         super().__init__()
-        self.model = A2C.load("./TrainingInfo/models/A2C_CnnPolicy/model_at_1000000")
+        self.model = ACER.load("./TrainingInfo/models/A2C_CnnPolicy/model_at_1000000")
     def getAction(self,env:Env,lastobservation,lastreward):
         action, _states = self.model.predict(lastobservation, deterministic=True)
         return action
