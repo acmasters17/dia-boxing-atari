bb96b0250f20a1d487b23c1f73e01ee508ce8c54
Adam <mastersacm17@gmail.com>
2022-05-02 20:28:28 +0100

    Preparing DQN run

--- mainrltrain.py
+++ mainrltrain.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from Utilities.modelSaver import SaveOnBestTrainingRewardCallback
 from stable_baselines3.common import results_plotter
-from stable_baselines3 import A2C, TD3
+from stable_baselines3 import A2C, DQN, TD3
 from stable_baselines3.common.results_plotter import plot_results
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
@@ -20,14 +20,14 @@ LOCAL_LOGS_DIR = './TrainingInfo/local_logs'
 TENSORBOARD_LOGS_DIR = './TrainingInfo/tensorboard_logs/'
 TIMESTAMPS = 5000000
 os.makedirs(LOCAL_LOGS_DIR, exist_ok=True)
-callback = SaveOnBestTrainingRewardCallback(check_freq=2500,save_path=CHECKPOINT_DIR, algorithm_name="A2C_CnnPolicy")
+callback = SaveOnBestTrainingRewardCallback(check_freq=2500,save_path=CHECKPOINT_DIR, algorithm_name="DQN_CnnPolicy")
 env = make_atari_env('BoxingNoFrameskip-v4', n_envs=4, seed=0, monitor_dir=LOCAL_LOGS_DIR)
 # Frame-stacking with 4 frames
 env = VecFrameStack(env, n_stack=4)
-model = A2C('CnnPolicy', env, verbose=1, tensorboard_log=TENSORBOARD_LOGS_DIR)
+model = DQN('CnnPolicy', env, verbose=1, tensorboard_log=TENSORBOARD_LOGS_DIR)
 model.learn(total_timesteps=TIMESTAMPS, callback=callback)
 
-plot_results([LOCAL_LOGS_DIR], TIMESTAMPS, results_plotter.X_TIMESTEPS, "A2C_CnnPolicy Boxing Over 5 million training cycles")
+plot_results([LOCAL_LOGS_DIR], TIMESTAMPS, results_plotter.X_TIMESTEPS, "DQN_CnnPolicy Boxing Over 5 million training cycles")
 plt.show()
 exit(0)
 
