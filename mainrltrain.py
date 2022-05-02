6db87e9f38ea5e77e312fbc6ee2dd41aef74ee7f
Adam <mastersacm17@gmail.com>
2022-05-02 20:36:27 +0100

    Prep for PPO

--- mainrltrain.py
+++ mainrltrain.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from Utilities.modelSaver import SaveOnBestTrainingRewardCallback
 from stable_baselines3.common import results_plotter
-from stable_baselines3 import A2C, DQN, TD3
+from stable_baselines3 import A2C, DQN, PPO, TD3
 from stable_baselines3.common.results_plotter import plot_results
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
@@ -18,16 +18,15 @@ from stable_baselines3.common.vec_env import VecFrameStack
 CHECKPOINT_DIR = './TrainingInfo/'
 LOCAL_LOGS_DIR = './TrainingInfo/local_logs'
 TENSORBOARD_LOGS_DIR = './TrainingInfo/tensorboard_logs/'
-TIMESTAMPS = 5000000
+TIMESTAMPS = 100000
 os.makedirs(LOCAL_LOGS_DIR, exist_ok=True)
-callback = SaveOnBestTrainingRewardCallback(check_freq=2500,save_path=CHECKPOINT_DIR, algorithm_name="DQN_CnnPolicy")
-env = make_atari_env('BoxingNoFrameskip-v4', n_envs=2, seed=0, monitor_dir=LOCAL_LOGS_DIR)
-# Frame-stacking with 4 frames
-env = VecFrameStack(env, n_stack=2)
-model = DQN('CnnPolicy', env, verbose=1, tensorboard_log=TENSORBOARD_LOGS_DIR)
+callback = SaveOnBestTrainingRewardCallback(check_freq=25000,save_path=CHECKPOINT_DIR, algorithm_name="PPO_CnnPolicy")
+env = make_atari_env('BoxingNoFrameskip-v4', n_envs=4, seed=0, monitor_dir=LOCAL_LOGS_DIR)
+env = VecFrameStack(env,n_stack=4)
+model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=TENSORBOARD_LOGS_DIR)
 model.learn(total_timesteps=TIMESTAMPS, callback=callback)
 
-plot_results([LOCAL_LOGS_DIR], TIMESTAMPS, results_plotter.X_TIMESTEPS, "DQN_CnnPolicy Boxing Over 5 million training cycles")
+plot_results([LOCAL_LOGS_DIR], TIMESTAMPS, results_plotter.X_TIMESTEPS, "PPO_CnnPolicy Boxing Over 5 million training cycles")
 plt.show()
 exit(0)
 
